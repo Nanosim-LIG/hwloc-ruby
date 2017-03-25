c841e3b333a14eb6630725fe054a869235b31ffd
Brice Videau <brice.videau@imag.fr>
2017-03-25 18:58:08 +0100

    More bitmap.

--- lib/hwloc/Bitmap.rb
+++ lib/hwloc/Bitmap.rb
@@ -27,6 +27,8 @@ module Hwloc
 
   attach_function :hwloc_bitmap_singlify, [:bitmap], :void
 
+  attach_function :hwloc_bitmap_to_ulong, [:bitmap], :ulong
+  attach_function :hwloc_bitmap_to_ith_ulong, [:bitmap, :uint], :ulong
   attach_function :hwloc_bitmap_isset, [:bitmap, :uint], :int
   attach_function :hwloc_bitmap_iszero, [:bitmap], :int
   attach_function :hwloc_bitmap_isfull, [:bitmap], :int
@@ -35,10 +37,17 @@ module Hwloc
   attach_function :hwloc_bitmap_last, [:bitmap], :int
   attach_function :hwloc_bitmap_weight, [:bitmap], :int
 
-  attach_function :hwloc_bitmap_and, [:bitmap, :bitmap, :bitmap], :void
   attach_function :hwloc_bitmap_or, [:bitmap, :bitmap, :bitmap], :void
+  attach_function :hwloc_bitmap_and, [:bitmap, :bitmap, :bitmap], :void
+  attach_function :hwloc_bitmap_andnot, [:bitmap, :bitmap, :bitmap], :void
   attach_function :hwloc_bitmap_xor, [:bitmap, :bitmap, :bitmap], :void
   attach_function :hwloc_bitmap_not, [:bitmap, :bitmap], :void
+
+  attach_function :hwloc_bitmap_intersects, [:bitmap, :bitmap], :int
+  attach_function :hwloc_bitmap_isincluded, [:bitmap, :bitmap], :int
+  attach_function :hwloc_bitmap_isequal, [:bitmap, :bitmap], :int
+  attach_function :hwloc_bitmap_compare_first, [:bitmap, :bitmap], :int
+  attach_function :hwloc_bitmap_compare, [:bitmap, :bitmap], :int
 
   class Bitmap
     attr_reader :ptr
@@ -69,8 +78,15 @@ module Hwloc
           }
           str = list.join(",")
           s_ptr = FFI::MemoryPointer::from_string(str)
-          p s_ptr
-          p s_ptr.read_string
+          @ptr = FFI::AutoPointer::new( Hwloc.hwloc_bitmap_alloc, Hwloc.method(:hwloc_bitmap_free) )
+          Hwloc.hwloc_bitmap_list_sscanf(@ptr,s_ptr)
+        elsif arg.kind_of?( Range ) then
+          if arg.last == Float::INFINITY then
+            str = "#{arg.first}-"
+          else
+            str = "#{arg.first}-#{arg.last - (arg.exclude_end? ? 1 : 0)}"
+          end
+          s_ptr = FFI::MemoryPointer::from_string(str)
           @ptr = FFI::AutoPointer::new( Hwloc.hwloc_bitmap_alloc, Hwloc.method(:hwloc_bitmap_free) )
           Hwloc.hwloc_bitmap_list_sscanf(@ptr,s_ptr)
         end
@@ -116,6 +132,8 @@ module Hwloc
       Hwloc.hwloc_bitmap_zero(@ptr)
       return self
     end
+
+    alias clear zero!
 
     def fill!
       Hwloc.hwloc_bitmap_fill(@ptr)
@@ -188,6 +206,8 @@ module Hwloc
       end
     end
 
+    alias empty? zero?
+
     def full?
       if Hwloc.hwloc_bitmap_isfull(@ptr) != 0 then
         return true
@@ -220,6 +240,8 @@ module Hwloc
       return w
     end
 
+    alias size weight
+
     def each
       if block_given? then
         indx = -1
@@ -238,11 +260,17 @@ module Hwloc
       return res
     end
 
+    alias intersection &
+
     def |(other)
       res = Bitmap::new
       Hwloc.hwloc_bitmap_or(res.ptr, @ptr, other.ptr)
       return res
     end
+
+    alias + |
+
+    alias union |
 
     def ^(other)
       res = Bitmap::new
@@ -254,6 +282,52 @@ module Hwloc
       res = Bitmap::new
       Hwloc.hwloc_bitmap_not(res.ptr, @ptr)
       return res
+    end
+
+    def -(other)
+      res = Bitmap::new
+      Hwloc.hwloc_bitmap_andnot(res.ptr, @ptr, other.ptr)
+      return res
+    end
+
+    def ==(other)
+      return Hwloc.hwloc_bitmap_isequal(@ptr, other.ptr) != 0
+    end
+
+    def include?(other)
+      return Hwloc.hwloc_bitmap_isincluded(other.ptr, @ptr) != 0
+    end
+
+    alias >= include?
+
+    def >(other)
+      return self >= other && !(self == other)
+    end
+
+    def included?(other)
+      return Hwloc.hwloc_bitmap_isincluded(@ptr, other.ptr) != 0
+    end
+
+    alias <= included?
+
+    def <(other)
+      return self <= other && !(self == other)
+    end
+
+    def intersect?(other)
+      return Hwloc.hwloc_bitmap_intersects(@ptr, other.ptr) != 0
+    end
+
+    def disjoint?(other)
+      return Hwloc.hwloc_bitmap_intersects(@ptr, other.ptr) == 0
+    end
+
+    def compare_first(other)
+      return Hwloc.hwloc_bitmap_compare_first(@ptr, other.ptr) == 0
+    end
+
+    def compare(other)
+      return Hwloc.hwloc_bitmap_compare(@ptr, other.ptr) == 0
     end
 
   end
