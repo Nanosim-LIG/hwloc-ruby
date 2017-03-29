06a59654830dc56c94626083fb9975f4e241409e
Brice Videau <brice.videau@imag.fr>
2017-03-29 17:38:57 +0200

    Changed BindError to CpubindError.

--- lib/hwloc/Bind.rb
+++ lib/hwloc/Bind.rb
@@ -18,58 +18,61 @@ module Hwloc
   class BindError < Error
   end
 
+  class CpubindError < BindError
+  end
+
   class Topology
 
     def set_cpubind(cpuset, flags)
       err = Hwloc.hwloc_set_cpubind(@ptr, cpuset, flags)
-      raise BindError if err == -1
+      raise CpubindError if err == -1
       return self
     end
 
     def get_cpubind(flags)
       cpuset = Cpuset::new
       err = Hwloc.hwloc_get_cpubind(@ptr, cpuset, flags)
-      raise BindError if err == -1
+      raise CpubindError if err == -1
       return cpuset
     end
 
     def set_proc_cpubind(pid, cpuset, flags)
       err = Hwloc.hwloc_set_proc_cpubind(@ptr, pid, cpuset, flags)
-      raise BindError if err == -1
+      raise CpubindError if err == -1
       return self
     end
 
     def get_proc_cpubind(pid, flags)
       cpuset = Cpuset::new
       err = Hwloc.hwloc_get_proc_cpubind(@ptr, pid, cpuset, flags)
-      raise BindError if err == -1
+      raise CpubindError if err == -1
       return cpuset
     end
 
     def set_thread_cpubind(thread, cpuset, flags)
       err = Hwloc.hwloc_set_thread_cpubind(@ptr, thread, cpuset, flags)
-      raise BindError if err == -1
+      raise CpubindError if err == -1
       return self
     end
 
     def get_thread_cpubind(thread, flags)
       cpuset = Cpuset::new
       err = Hwloc.hwloc_get_thread_cpubind(@ptr, thread, cpuset, flags)
-      raise BindError if err == -1
+      raise CpubindError if err == -1
       return cpuset
     end
 
     def get_last_cpu_location(flags)
       cpuset = Cpuset::new
       err = Hwloc.hwloc_get_last_cpu_location(@ptr, cpuset, flags)
-      raise BindError if err == -1
+      raise CpubindError if err == -1
       return cpuset
     end
 
     def get_proc_last_cpu_location(pid, flags)
       cpuset = Cpuset::new
       err = Hwloc.hwloc_get_proc_last_cpu_location(@ptr, pid, cpuset, flags)
-      raise BindError if err == -1
+      raise CpubindError if err == -1
       return cpuset
     end
 
