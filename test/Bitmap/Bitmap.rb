b15a17054972b0fe4a25791cb84c345cc6869166
Brice Videau <brice.videau@imag.fr>
2017-04-04 09:36:21 +0200

    More Bitmap tests.

--- test/Bitmap/Bitmap.rb
+++ test/Bitmap/Bitmap.rb
@@ -30,10 +30,102 @@ class BitmapTest < Minitest::Test
 
   def test_accessor
     b = Hwloc::Bitmap::new("0x14")
-    assert_equal( false, b[3] )
+    assert_equal(false, b[3])
     b[3] = true
-    assert_equal( true, b[3] )
-    assert_equal( [2..4], b.to_a)
+    assert_equal(true, b[3])
+    assert_equal([2..4], b.to_a)
+  end
+
+  def test_weight
+    b = Hwloc::Bitmap::new("0x14")
+    assert_equal(2, b.weight)
+    b = Hwloc::Bitmap::new([1,2,5,7,11,15..Float::INFINITY])
+    assert_equal(Float::INFINITY, b.weight)
+  end
+
+  def test_each
+    b = Hwloc::Bitmap::new("0x14")
+    assert_equal([2,4], b.each.to_a)
+    b = Hwloc::Bitmap::new([1,2,5,7,11,15..Float::INFINITY])
+    assert_equal([1,2,5,7,11,15,16], b.each.lazy.take(7).force)
+  end
+
+  def test_zero
+    b = Hwloc::Bitmap::new
+    assert(b.zero?)
+    b = Hwloc::Bitmap::new("0x14")
+    assert(!b.zero?)
+    b.zero!
+    assert(b.zero?)
+  end
+
+  def test_fill_full
+    b = Hwloc::Bitmap::new("0x14")
+    assert(!b.full?)
+    b.fill!
+    assert(b.full?)
+  end
+
+  def test_only_all_but
+    b = Hwloc::Bitmap::new
+    b.only!(72)
+    assert_equal(1, b.weight)
+    assert_equal([72], b.to_a)
+    b.all_but!(72)
+    assert_equal(Float::INFINITY, b.weight)
+    assert_equal([0..71, 73..Float::INFINITY], b.to_a)
+    b.singlify!
+    assert_equal([0], b.to_a)
+  end
+
+  def test_operators
+    b1 = Hwloc::Bitmap::new
+    b1.only!(72)
+    b2 = Hwloc::Bitmap::new
+    b2.all_but!(72)
+    assert((b1 & b2).empty?)
+    assert((b1 | b2).full?)
+    assert((b1 + b2).full?)
+    b3 = Hwloc::Bitmap::new
+    b3.fill!
+    assert((b3 - b1) == b2)
+    assert((b3 ^ b1) == b2)
+    assert(~(b3 - b1) == b1)
+  end
+
+  def test_ensemble
+    b1 = Hwloc::Bitmap::new([17...25])
+    b2 = Hwloc::Bitmap::new([19..20])
+    assert(b1.include?(b2))
+    assert(b1 >= b2)
+    assert(b1 > b2)
+    assert(b2.included?(b1))
+    assert(b2 <= b1)
+    assert(b2 < b1)
+    assert(!(b1 < b1))
+    assert(b1 <= b1)
+    assert(b1.intersect?(b2))
+    b3 = Hwloc::Bitmap::new([19..26])
+    assert(!b1.include?(b3))
+    assert(b1.intersect?(b3))
+    b4 = Hwloc::Bitmap::new([25..30])
+    assert(!b1.include?(b4))
+    assert(!b1.intersect?(b4))
+    assert(b1.disjoint?(b4))
+  end
+
+  def test_compare
+    b1 = Hwloc::Bitmap::new([17...25])
+    b2 = Hwloc::Bitmap::new([19..20])
+    assert(b1.compare_first(b2) < 0)
+    assert(b2.compare_first(b1) > 0)
+    assert(b1.compare_first(b1) == 0)
+    assert(b1.compare(b2) ==  1)
+    assert(b2.compare(b1) == -1)
+    assert(b1.compare(b1) ==  0)
+    assert((b1 <=> b2) ==  1)
+    assert((b2 <=> b1) == -1)
+    assert((b1 <=> b1) ==  0)
   end
 
 end
